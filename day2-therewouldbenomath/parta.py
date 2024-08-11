8004a1fe7f69a71c4c20de41ceffd0fed836af3a
uma <umarani.pamu@outlook.com>
2024-08-11 22:13:38 +0530

    made changes

--- day2-therewouldbenomath/parta.py
+++ day2-therewouldbenomath/parta.py
@@ -1,6 +1,6 @@
     
 def calculate_wrapping(length, width, height):
-    surfaceArea = 2 * length * width + 2 * width * height + 2 * height * length
+    surfaceArea = 2 * (length * width + width * height + height * length)
     smallest_side = min(length * width, width * height, height * length)
     return surfaceArea + smallest_side
 
